f782e13878757836b97874800426ab2708c7ab89
Florian Hinrichsen <florian.hinrichsen@werum.com>
2023-05-10 08:25:29 +0200

    Update commons.php

--- commons.php
+++ commons.php
@@ -9,7 +9,7 @@ define("LOG_LEVEL_FATAL",0);
 define("LOG_LEVEL_ERROR",1);
 define("LOG_LEVEL_WARN",2);
 define("LOG_LEVEL_INFO",3);
-define("LOG_LEVEL_DEBUG",3);
+define("LOG_LEVEL_DEBUG",4);
 define("LOG_LEVEL_JOB",5);
 
 
